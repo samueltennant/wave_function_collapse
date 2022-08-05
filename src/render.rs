db0c22225dc71b365db8b08d13f715091134edba
Samuel Tennant <samueltennant@hotmail.com>
2022-08-05 21:38:55 +0100

    Read image file to struct

--- src/render.rs
+++ src/render.rs
@@ -3,7 +3,14 @@ use image::{
     DynamicImage, ImageBuffer, ImageError, ImageFormat, Rgb,
 };
 
+#[derive(Debug)]
 pub struct ImageDimensions(pub u32, pub u32);
+
+#[derive(Debug)]
+pub struct Image {
+    pub dimensions: ImageDimensions,
+    pub colors: Vec<Rgb<u8>>,
+}
 
 pub fn output_image(colors: Vec<Rgb<u8>>, dim: ImageDimensions) -> Result<(), ImageError> {
     // Ensure that the colors vector is the correct length.
@@ -23,10 +30,24 @@ pub fn output_image(colors: Vec<Rgb<u8>>, dim: ImageDimensions) -> Result<(), Im
     Ok(())
 }
 
-// Honestly thought this would need to do more.
-// Don't care, not going to move it.
-pub fn read_image(path: &str) -> Result<DynamicImage, ImageError> {
+pub fn read_image(path: &str) -> Result<Image, ImageError> {
     let image = image::open(path)?;
 
-    Ok(image)
+    let image = match image {
+        DynamicImage::ImageRgb8(image) => image,
+        _ => {
+            return Err(ImageError::Encoding(EncodingError::from_format_hint(
+                ImageFormatHint::Exact(ImageFormat::Png),
+            )))
+        }
+    };
+
+    let dimensions = ImageDimensions(image.width(), image.height());
+    let mut colors = Vec::new();
+
+    for pixel in image.pixels() {
+        colors.push(Rgb::from(pixel.0));
+    }
+
+    Ok(Image { dimensions, colors })
 }
